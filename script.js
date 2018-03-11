472bb7391c7e963d233c133ba0f5b0359a6ccbbe
fish501 <levi.fisher@hotmail.com>
2018-03-11 16:10:42 +0800

    background image update for smaller screens

---
+++ script.js
@@ -0,0 +1,8 @@
+
+defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"
+
+$(document).ready(function() {
+			$('.toggle').click(function(){
+				$('ul').toggleClass('active');
+			})
+		})
